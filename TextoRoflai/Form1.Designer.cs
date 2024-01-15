29beb43c11953c9a164b0bab1a5033552f18965f
tfb123 <mil.xomiak@gmail.com>
2024-01-15 03:40:52 +0200

    Add project files.

---
+++ TextoRoflai/Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace TextoRoflai
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SaveButton = new System.Windows.Forms.Button();
+            this.AppendButton = new System.Windows.Forms.Button();
+            this.SplitButton = new System.Windows.Forms.Button();
+            this.SubstringButton = new System.Windows.Forms.Button();
+            this.SearchButton = new System.Windows.Forms.Button();
+            this.OpenButton = new System.Windows.Forms.Button();
+            this.btnRaides = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(17, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(972, 473);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // SaveButton
+            // 
+            this.SaveButton.Location = new System.Drawing.Point(17, 501);
+            this.SaveButton.Name = "SaveButton";
+            this.SaveButton.Size = new System.Drawing.Size(142, 34);
+            this.SaveButton.TabIndex = 1;
+            this.SaveButton.Text = "Save";
+            this.SaveButton.UseVisualStyleBackColor = true;
+            this.SaveButton.Click += new System.EventHandler(this.SaveButton_Click);
+            // 
+            // AppendButton
+            // 
+            this.AppendButton.Location = new System.Drawing.Point(165, 501);
+            this.AppendButton.Name = "AppendButton";
+            this.AppendButton.Size = new System.Drawing.Size(142, 34);
+            this.AppendButton.TabIndex = 2;
+            this.AppendButton.Text = "Append";
+            this.AppendButton.UseVisualStyleBackColor = true;
+            this.AppendButton.Click += new System.EventHandler(this.AppendButton_Click);
+            // 
+            // SplitButton
+            // 
+            this.SplitButton.Location = new System.Drawing.Point(313, 501);
+            this.SplitButton.Name = "SplitButton";
+            this.SplitButton.Size = new System.Drawing.Size(142, 34);
+            this.SplitButton.TabIndex = 3;
+            this.SplitButton.Text = "Split";
+            this.SplitButton.UseVisualStyleBackColor = true;
+            this.SplitButton.Click += new System.EventHandler(this.SplitButton_Click);
+            // 
+            // SubstringButton
+            // 
+            this.SubstringButton.Location = new System.Drawing.Point(461, 501);
+            this.SubstringButton.Name = "SubstringButton";
+            this.SubstringButton.Size = new System.Drawing.Size(142, 34);
+            this.SubstringButton.TabIndex = 4;
+            this.SubstringButton.Text = "Substring";
+            this.SubstringButton.UseVisualStyleBackColor = true;
+            this.SubstringButton.Click += new System.EventHandler(this.SubstringButton_Click);
+            // 
+            // SearchButton
+            // 
+            this.SearchButton.Location = new System.Drawing.Point(609, 501);
+            this.SearchButton.Name = "SearchButton";
+            this.SearchButton.Size = new System.Drawing.Size(142, 34);
+            this.SearchButton.TabIndex = 5;
+            this.SearchButton.Text = "Search";
+            this.SearchButton.UseVisualStyleBackColor = true;
+            this.SearchButton.Click += new System.EventHandler(this.SearchButton_Click);
+            // 
+            // OpenButton
+            // 
+            this.OpenButton.Location = new System.Drawing.Point(757, 501);
+            this.OpenButton.Name = "OpenButton";
+            this.OpenButton.Size = new System.Drawing.Size(142, 34);
+            this.OpenButton.TabIndex = 6;
+            this.OpenButton.Text = "Open";
+            this.OpenButton.UseVisualStyleBackColor = true;
+            this.OpenButton.Click += new System.EventHandler(this.OpenButton_Click);
+            // 
+            // btnRaides
+            // 
+            this.btnRaides.Location = new System.Drawing.Point(905, 501);
+            this.btnRaides.Name = "btnRaides";
+            this.btnRaides.Size = new System.Drawing.Size(142, 34);
+            this.btnRaides.TabIndex = 7;
+            this.btnRaides.Text = "Raides";
+            this.btnRaides.UseVisualStyleBackColor = true;
+            this.btnRaides.Click += new System.EventHandler(this.btnRaides_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1090, 547);
+            this.Controls.Add(this.btnRaides);
+            this.Controls.Add(this.OpenButton);
+            this.Controls.Add(this.SearchButton);
+            this.Controls.Add(this.SubstringButton);
+            this.Controls.Add(this.SplitButton);
+            this.Controls.Add(this.AppendButton);
+            this.Controls.Add(this.SaveButton);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button SaveButton;
+        private System.Windows.Forms.Button AppendButton;
+        private System.Windows.Forms.Button SplitButton;
+        private System.Windows.Forms.Button SubstringButton;
+        private System.Windows.Forms.Button SearchButton;
+        private System.Windows.Forms.Button OpenButton;
+        private System.Windows.Forms.Button btnRaides;
+    }
+}
+
---
+++ TextoRoflai/Form1.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TextoRoflai
+{
+    public partial class Form1 : Form
+    {
+        FileStream fileStream;
+        Byte[] dataBytes;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void SaveButton_Click(object sender, EventArgs e)
+        {
+            if(richTextBox1.Text.Length>0)
+            {
+                SaveFileDialog sfd = new SaveFileDialog();
+                sfd.Title = "Select where to save text file";
+                sfd.Filter = "TXT document|*.txt";
+                if (sfd.ShowDialog() == DialogResult.OK)
+                {
+                    fileStream = new FileStream(sfd.FileName, FileMode.OpenOrCreate);
+                    dataBytes = Encoding.ASCII.GetBytes(richTextBox1.Text);
+                    fileStream.Write(dataBytes, 0, dataBytes.Length);
+                    fileStream.Close();
+                }
+            }
+        }
+
+        private void OpenButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                StreamReader streamReader;
+                string line;
+                OpenFileDialog opf = new OpenFileDialog();
+                opf.Title = "Select File to Open";
+                opf.Filter = "TXT document|*.txt";
+                if (opf.ShowDialog() == DialogResult.OK)
+                {
+                    richTextBox1.Text = "";
+                    streamReader = new StreamReader(opf.FileName);
+                    line = streamReader.ReadToEnd();
+                    richTextBox1.Text = line;
+                    streamReader.Close();
+                }
+            } catch (Exception ex)
+            {
+                MessageBox.Show("Error reading file: " + ex.StackTrace);
+            }
+        }
+
+        private void AppendButton_Click(object sender, EventArgs e)
+        {
+            if (richTextBox1.Text.Length > 0)
+            {
+                SaveFileDialog sfd = new SaveFileDialog();
+                sfd.Title = "Select where to save text file";
+                sfd.Filter = "TXT document|*.txt";
+                if (sfd.ShowDialog() == DialogResult.OK)
+                {
+                    fileStream = new FileStream(sfd.FileName, FileMode.Append);
+                    dataBytes = Encoding.ASCII.GetBytes(richTextBox1.Text);
+                    fileStream.Write(dataBytes, 0, dataBytes.Length);
+                    fileStream.Close();
+                }
+            }
+        }
+
+        private void SplitButton_Click(object sender, EventArgs e)
+        {
+            string text;
+            string[] words;
+            if(richTextBox1.Text.Length>0)
+            {
+                text = richTextBox1.Text;
+                words = text.Split(' ');
+                richTextBox1.Text = "";
+                foreach(string item in words)
+                {
+                    richTextBox1.Text += item + Environment.NewLine;
+                }
+            }
+        }
+
+        private void SubstringButton_Click(object sender, EventArgs e)
+        {
+            string text;
+            if (richTextBox1.Text.Length >4 )
+            {
+                text = richTextBox1.Text;
+                richTextBox1.Text = "";
+                richTextBox1.Text = Text.Substring(0, 3);
+            }
+        }
+
+        private void SearchButton_Click(object sender, EventArgs e)
+        {
+            string text;
+            if (richTextBox1.Text.Length>0)
+            {
+                text = richTextBox1.Text;
+                string ats = Microsoft.VisualBasic.Interaction.InputBox("Введите текст который хотите найти");
+                if (text.Contains(ats))
+                {
+                    int pos = text.IndexOf(ats);
+                    richTextBox1.Focus();
+                    richTextBox1.Select(pos,ats.Length);
+                }
+                else
+                {
+                    MessageBox.Show("Такой фразы нет");
+                }
+            }
+        }
+
+        private void btnRaides_Click(object sender, EventArgs e)
+        {
+            var ofd = new OpenFileDialog();
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                FileStream fs = new FileStream(ofd.FileName, FileMode.Open, FileAccess.Read);
+                byte[] raides = new byte[fs.Length];
+                fs.Read(raides, 0, raides.Length);
+                int a = 0;
+                int A = 0;
+                int b = 0;
+                int B = 0;
+                foreach (byte item in raides)
+                {
+                    if ((char)item == 'a') a++;
+                    else if ((char)item == 'A') A++;
+                    else if ((char)item == 'b') b++;
+                    else if ((char)item == 'B') B++;
+                }
+                var sfd = new SaveFileDialog();
+                if (sfd.ShowDialog() == DialogResult.OK)
+                {
+                    StreamWriter writer = new StreamWriter(sfd.FileName);
+                    writer.WriteLine("a букв: " + a + "\n");
+                    writer.WriteLine("A букв: " + A + "\n");
+                    writer.WriteLine("b букв: " + b + "\n");
+                    writer.WriteLine("B букв: " + B + "\n");
+                    writer.Close();
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
